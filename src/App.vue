1dcfeae6b2f120232298ff4dd5535598d6ec42eb
Prashanga <prashanga@gmail.com>
2020-04-22 18:18:02 +1000

    Building questions

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox :question="questions[index]" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuestionBox from "./components/QuestionBox"
+import Header from "./components/Header"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    QuestionBox,
+    Header,
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+    }
+  },
+  mounted() {
+    fetch("https://opentdb.com/api.php?amount=10&category=17&type=multiple", {
+      method: "get",
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((res) => {
+        this.questions = res.results
+      })
+  },
 }
 </script>
 
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>Some wuedstione</template>
+      <hr class="my-4" />
+      <p>List of answers</p>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="primary" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: String,
+  },
+}
+</script>
+
+<style></style>
